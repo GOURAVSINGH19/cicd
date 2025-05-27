d1c838e0b91ee0ff9ba513b38a0231ebd7e771de
GOURAVSINGH19 <gouravsingh4495@gmail.com>
2025-05-27 18:00:16 +0530

    intialize-Docker

---
+++ app/page.test.ts
@@ -0,0 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import Home from "./page";
+it("App Router: Works with Server Components", () => {
+  render(<Home/>);
+  expect(screen.getByRole("heading")).toHaveTextContent<HTMLElement>("App Router");
+});
